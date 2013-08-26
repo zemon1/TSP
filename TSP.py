1d991ff9817788bb2634c751a6a8a9ce8eff8b47
Jeff Haak <zemon1@csh.rit.edu>
2013-08-26 12:36:28 -0400

    Initializes and randomizes the population

--- TSP.py
+++ TSP.py
@@ -1,11 +1,28 @@
 #Jeff Haak
 #Traveling Salesman Problem
+import copy, random
 
+def initializePop(popSize, chromoSize):
+     pop = []
+     count = 0
+     while count < popSize:
+          pop.append([count, [0] * chromoSize, 9999999])
+          count += 1
 
+     return pop
 
+def randomizePop(thePop):
+     choiceMax = len(thePop[0][1])
+     choices = []
+     for i in range(0, choiceMax):
+         choices.append(i)
 
-
-
+     for citizen in thePop:
+          availible = copy.deepcopy(choices)
+          random.shuffle(availible)
+          citizen[1] = availible         
+           
+     return thePop 
 
 
 
@@ -33,10 +50,14 @@ def readCities(fileName):
      return lines
 
 if __name__ == "__main__":
+     random.seed(1)
      cities = readCities("./cities.csv")
      structs = createDataStructs(cities)
-
-     for ele in structs[1]:
+     
+     pop = initializePop(10, 7)
+     pop = randomizePop(pop)
+     
+     for ele in pop:
          print ele
 
 
