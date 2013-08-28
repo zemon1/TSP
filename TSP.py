1508e46b0ddbba93a5b53e03f8776cbe06f63f88
Jeff Haak <zemon1@csh.rit.edu>
2013-08-28 16:25:03 -0400

    The GA is finished

--- TSP.py
+++ TSP.py
@@ -1,16 +1,18 @@
 #Jeff Haak
 #Traveling Salesman Problem
-import copy, random, numpy
+import copy, random, numpypy, numpy
 from operator import itemgetter
 
-global popId 
+popId = 0 
 
-
+#This function creates the initial population set
 def initializePop(popSize, chromoSize):
      pop = []
+     global popId
 
-     popId = 0
      count = 0
+     
+     #creates blank people for the program
      while count < popSize:
           pop.append([popId, [0] * chromoSize, 9999999])
           popId += 1
@@ -18,9 +20,14 @@ def initializePop(popSize, chromoSize):
 
      return pop
 
+#randomizes every person's chromosome in the population
 def randomizePop(thePop):
+     
+     #max number of options 
      choiceMax = len(thePop[0][1])
      choices = []
+
+     
      for i in range(0, choiceMax):
          choices.append(i)
 
@@ -42,16 +49,17 @@ def fitness(thePop, cityInfo):
           chromo = citizen[1]
           thisDist = [-1.0] * (chromoSize - 1)
           
+          #print "Chromo: ", chromo
+
           for i in range(chromoSize):
                cityOrder[i] = chromo.index(i)
-          
+                
           for i in range(len(cityOrder)):
                if not i == len(cityOrder) - 1:
                     thisCity = cityOrder[i]
                     nextCity = cityOrder[i+1]
 
                     thisDist[i] = distances[thisCity][nextCity]
-
 
           citizen[2] = sum(thisDist)
      
@@ -60,6 +68,7 @@ def fitness(thePop, cityInfo):
      return thePop
 
 def mate(thePop):
+     global popId
      result = [-1] * int(round(len(thePop) * .25)) 
      elites = [-1] * int(round(len(thePop) * .25)) 
      candidates = [-1] * int(round(len(thePop) * .555555))
@@ -73,35 +82,79 @@ def mate(thePop):
      count = 0
      while count < len(candidates):
          candidates[count] = thePop[count]
-         print candidates[count]
+         #print candidates[count]
          count += 1
      
-     print "\n"
+     #print "\n"
 
      empty = int(round(len(thePop) * .25)) 
      for i in range(len(candidates)):
           for j in range(len(candidates)):
                if i < j:
                     if empty < len(thePop):
-                         result.extend(crossOver(candidates[i], candidates[j]))
+                        result.extend(crossOver(candidates[i], candidates[j]))
 
-                         print candidates[i]
-                         print candidates[j]               
-                         print "---------" 
-                         empty += 2
+                        #print candidates[i]
+                        #print candidates[j]               
+                        #print "---------" 
+                    empty += 2
                               
 
-     print "\n"
+     #print "\n"
      while len(result) > len(thePop):
           result.pop()
-     
+          popId -= 1 
      return result
 
 def crossOver(par1, par2):
-     child1 = [-1, -1, -1]
-     child2 = [-1, -1, -1]
+     global popId
+     child1 = [popId, [], 9999999]
+     child2 = [popId + 1, [], 9999999]
+     
+     popId += 2
 
+     crossover = (len(par1[1]) % (random.randrange(25, 75, 1)/10) + 1) 
+     
+     count = 0
+     while count < crossover:
+          child1[1].append(par1[1][count])     
+          child2[1].append(par2[1][count])     
+          count += 1
+
+     count = 0
+     while count < len(par1[1]):
+          if len(child1[1]) < len(par1[1]):
+               try:
+                    child1[1].index(par2[1][count])
+               except:
+                    child1[1].append(par2[1][count])
+
+          if len(child2[1]) < len(par2[1]):
+               try:
+                    child2[1].index(par1[1][count])
+               except:
+                    child2[1].append(par1[1][count])
+
+          count += 1
+     
+     mutate(child1[1])
+     mutate(child2[1])
+     
      return [child1, child2]
+
+def mutate(chromo):
+     thresh = 17
+     
+     for i in range(0, len(chromo)):
+         chance = random.randrange(1, 100) 
+         
+         if chance <= thresh:
+             old = chromo[i]
+             ind = (i + 1) * -1
+             new = chromo[ind]
+             
+             chromo[i] = new
+             chromo[ind] = old
 
 def createDataStructs(theCities):
      cityOrder = {}
@@ -110,7 +163,8 @@ def createDataStructs(theCities):
      count = 0
 
      for city in theCities:
-          cityOrder[city[0]] = count
+          #cityOrder[city[0]] = count
+          cityOrder[count] = city[0]
           distances.append(city[1:])
           
           for i in range(len(distances)):
@@ -133,20 +187,78 @@ def readCities(fileName):
      
      return lines
 
-if __name__ == "__main__":
-     random.seed(1)
+def runGA(num = -1, close = 1):
      cities = readCities("./cities.csv")
      structs = createDataStructs(cities)
      
-     pop = initializePop(10, 7)
+     for city in structs[1]:
+         print city
+      
+     pop = initializePop(100, len(structs[1]))
      pop = randomizePop(pop)
-     
      pop = fitness(pop, structs)
+     
+     topDiff = 9999999
+     
+     print "Gen: ", 0
+     for i in range(len(pop)//10):
+         print pop[i]
 
-     pop = mate(pop)
 
-     for ele in pop:
-         print ele
+     if num == -1:
+         count = 0
+         while topDiff > close:
+             newPop = mate(pop)
+             pop = fitness(newPop, structs)
+            
+             cutoff = ((len(pop)//8) * 3) * -1
+             topDiff = pop[cutoff][2] - pop[0][2]
+             count += 1
+
+             print "Gen: ", count
+             for i in range(len(pop)//10):
+                 print pop[i]
+
+     else:
+         count = 0
+         while count < num:
+             newPop = mate(pop)
+             pop = fitness(newPop, structs)
+             
+             count += 1
+
+             print "Gen: ", count
+             for i in range(len(pop)//10):
+                 print pop[i]
+     
+     printSolution(pop[0], structs[0])
+
+                
+def printSolution(best, cities):
+     order = best[1]
+     dist = best[2]
+     
+     final = ""
+
+     for i in range(len(order)):
+         city = order.index(i)
+
+         final += cities[city] 
+         
+         if  i != len(order) - 1:
+             final += " -> "
+     
+     print "\n"
+     print final
+     print "\n" 
+
+      
+
+if __name__ == "__main__":
+     random.seed(1)
+     
+     runGA(-1)     
+
 
 
 
